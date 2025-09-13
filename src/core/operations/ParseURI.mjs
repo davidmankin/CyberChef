9bbc55fc9477d220586600273fafe800b3844e02
David Mankin <mankin@gmail.com>
2025-09-13 10:38:05 -0600

    Untested: attempt to add options to uri parsing

--- src/core/operations/ParseURI.mjs
+++ src/core/operations/ParseURI.mjs
@@ -24,7 +24,53 @@ class ParseURI extends Operation {
         this.infoURL = "https://wikipedia.org/wiki/Uniform_Resource_Identifier";
         this.inputType = "string";
         this.outputType = "string";
-        this.args = [];
+        this.args = [
+	    {
+		name: "Output as json",
+		type: "boolean",
+		value: false
+	    },
+	    {
+		name: "Include protocol",
+		type: "boolean",
+		value: true
+	    },
+	    {
+		name: "Include auth",
+		type: "boolean",
+		value: true
+	    },
+	    {
+		name: "Include hostname",
+		type: "boolean",
+		value: true
+	    },
+	    {
+		name: "Include port",
+		type: "boolean",
+		value: true
+	    },
+	    {
+		name: "Include path name",
+		type: "boolean",
+		value: true
+	    },
+	    {
+		name: "Include arguments (query)",
+		type: "boolean",
+		value: true
+	    },
+	    {
+		name: "Include hash",
+		type: "boolean",
+		value: true
+	    },
+	    {
+		name: "Parse hash like query string"
+		type: "boolean"
+		value: valse
+	    }
+	];
     }
 
     /**
@@ -34,34 +80,74 @@ class ParseURI extends Operation {
      */
     run(input, args) {
         const uri = url.parse(input, true);
+	const [as_json, include_protocol, include_auth, include_hostname, include_port, include_path, include_query, indlude_hash, parse_hash] = args;
 
         let output = "";
+	if (!as_json) {
 
-        if (uri.protocol) output += "Protocol:\t" + uri.protocol + "\n";
-        if (uri.auth) output += "Auth:\t\t" + uri.auth + "\n";
-        if (uri.hostname) output += "Hostname:\t" + uri.hostname + "\n";
-        if (uri.port) output += "Port:\t\t" + uri.port + "\n";
-        if (uri.pathname) output += "Path name:\t" + uri.pathname + "\n";
-        if (uri.query) {
-            const keys = Object.keys(uri.query);
-            let padding = 0;
+	    if (uri.protocol && include_protocol) output += "Protocol:\t" + uri.protocol + "\n";
+	    if (uri.auth && include_auth) output += "Auth:\t\t" + uri.auth + "\n";
+	    if (uri.hostname && include_hostname) output += "Hostname:\t" + uri.hostname + "\n";
+	    if (uri.port && include_port) output += "Port:\t\t" + uri.port + "\n";
+	    if (uri.pathname && include_path) output += "Path name:\t" + uri.pathname + "\n";
+	    if (uri.query && include_query) {
+		const keys = Object.keys(uri.query);
+		let padding = 0;
 
-            keys.forEach(k => {
-                padding = (k.length > padding) ? k.length : padding;
-            });
+		keys.forEach(k => {
+		    padding = (k.length > padding) ? k.length : padding;
+		});
 
-            output += "Arguments:\n";
-            for (const key in uri.query) {
-                output += "\t" + key.padEnd(padding, " ");
-                if (uri.query[key].length) {
-                    output += " = " + uri.query[key] + "\n";
-                } else {
-                    output += "\n";
-                }
-            }
-        }
-        if (uri.hash) output += "Hash:\t\t" + uri.hash + "\n";
+		output += "Arguments:\n";
+		for (const key in uri.query) {
+		    output += "\t" + key.padEnd(padding, " ");
+		    if (uri.query[key].length) {
+			output += " = " + uri.query[key] + "\n";
+		    } else {
+			output += "\n";
+		    }
+		}
+	    }
+	    if (uri.hash && include_hash) {
+		if (!parse_hash) {
+		    output += "Hash:\t\t" + uri.hash + "\n";
+		} else {
+		    parsed_hash = decode_hash(uri.hash)
+		    const keys = Object.keys(parsed_hash);
+		    let padding = 0;
 
+		    keys.forEach(k => {
+			padding = (k.length > padding) ? k.length : padding;
+		    });
+
+		    output += "Hash (parsed):\n";
+		    for (const key in uri.query) {
+			output += "\t" + key.padEnd(padding, " ");
+			if (parsed_hash[key].length) {
+			    output += " = " + parsed_hash[key] + "\n";
+			} else {
+			    output += "\n";
+			}
+		    }
+		}
+	    }
+
+	} else { // JSON
+	    let obj = {}
+	    if (include_protocol) obj.protocol = uri.protocol;
+	    if (include_auth) obj.auth = uri.auth;
+	    if (include_hostname) obj.hostname = url.hostname;
+	    if (include_port) obj.port = uri.port;
+	    if (include_path) obj.pathname = uri.pathname;
+	    if (include_query) obj.query = uri.query
+	    if (include_hash) {
+		if (parse_hash && uri.hash) {
+		    obj.hash = decode_hash(uri.hash);
+		} else {
+		    obj.hash = uri.hash;
+		}
+	    }
+	}
         return output;
     }
 
